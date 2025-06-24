6677dbacbea22c4a1ded1fbf08059756a70a04a4
Florian <112929561+guerrinflorian@users.noreply.github.com>
2025-06-24 08:49:36 +0000

    modif pour vercel

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-import Fastify from 'fastify';
-import { createClient } from '@supabase/supabase-js';
-import { hashPassword, comparePassword } from './passwordUtils.js';
-import FastifyJwt from '@fastify/jwt';
-import FastifyCors from '@fastify/cors';
-import user from './routes/users.js';
-import adminUsers from './routes/adminUsers.js';
-import club from './routes/clubs.js';
-import { supabaseUrl, supabaseAnonKey, jwtSecret } from './config.js';
-import tournamentStatus from './routes/tournamentStatus.js';
-import tournament from './routes/tournament.js';
-import category from './routes/category.js';
-import participants from './routes/participants.js';
-import tournamentComments from './routes/tournamentComments.js'
-
+import Fastify from "fastify";
+import { createClient } from "@supabase/supabase-js";
+import { hashPassword, comparePassword } from "./passwordUtils.js";
+import FastifyJwt from "@fastify/jwt";
+import FastifyCors from "@fastify/cors";
+import user from "./routes/users.js";
+import adminUsers from "./routes/adminUsers.js";
+import club from "./routes/clubs.js";
+import { supabaseUrl, supabaseAnonKey, jwtSecret } from "./config.js";
+import tournamentStatus from "./routes/tournamentStatus.js";
+import tournament from "./routes/tournament.js";
+import category from "./routes/category.js";
+import participants from "./routes/participants.js";
+import tournamentComments from "./routes/tournamentComments.js";
 
 // init de Fastify
 const fastify = Fastify({ logger: true });
@@ -21,17 +20,16 @@ const fastify = Fastify({ logger: true });
 fastify.register(FastifyJwt, {
   secret: jwtSecret,
   sign: {
-    expiresIn: '4h'     // le token expire 2 heures apres sa création
-  }
+    expiresIn: "4h", // le token expire 2 heures apres sa création
+  },
 });
 
 fastify.register(FastifyCors, {
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 });
-
 
 // connexion à Supabase
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
@@ -43,7 +41,12 @@ fastify.register(category, { supabase, verifyJWT });
 fastify.register(tournamentStatus, { supabase, verifyJWT });
 fastify.register(participants, { supabase, verifyJWT });
 fastify.register(user, { supabase, hashPassword, comparePassword, verifyJWT });
-fastify.register(adminUsers, { supabase, hashPassword, comparePassword, verifyJWT });
+fastify.register(adminUsers, {
+  supabase,
+  hashPassword,
+  comparePassword,
+  verifyJWT,
+});
 fastify.register(tournamentComments, { supabase, verifyJWT });
 
 async function verifyJWT(request, reply) {
@@ -55,14 +58,20 @@ async function verifyJWT(request, reply) {
 }
 
 // laancer le serveur
-const start = async () => {
-  try {
-    await fastify.listen({ port: 3000, host: '0.0.0.0' });
-    fastify.log.info(`🚀 Serveur lancé`);
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-};
+//const start = async () => {
+//  try {
+//    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+//   fastify.log.info(`🚀 Serveur lancé`);
+// } catch (err) {
+//   fastify.log.error(err);
+//    process.exit(1);
+// }
+//};
 
-start();
+//start();
+
+// Ajouter à la place :
+export default async function handler(req, res) {
+  await fastify.ready();
+  fastify.server.emit("request", req, res);
+}
